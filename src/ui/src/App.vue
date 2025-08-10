09da95dd2f0653aec8e77304debea123535ea18d
Dolphin xyz <bharat.raghavan03@gmail.com>
2025-08-09 20:06:24 -0400

    Create static output component Dynamic component will be created once fast iteration is possible (frontend and backend must be linked)

--- src/ui/src/App.vue
+++ src/ui/src/App.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import InputDialogBox from './components/InputDialogBox.vue';
+import OutputDialogBox from './components/OutputDialogBox.vue';
 </script>
 
 <template>
   <div id="baseboard" className="grid grid-cols-2 h-screen w-screen">
-    <div id="inputs" className="flex items-center justify-center">
+    <div id="inputs" className="flex items-center justify-center ml-20">
       <InputDialogBox />
     </div>
-    <div id="outputs" className="flex items-center justify-center">
-      <p>hello world 2</p>
+    <div id="outputs" className="flex items-center justify-center mr-20">
+      <OutputDialogBox />
     </div>
   </div>
 </template>
---
+++ src/ui/src/components/OutputDialogBox.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import { PhLegoSmiley } from '@phosphor-icons/vue';
+
+</script>
+
+<template>
+    <div className="h-3/4 w-1/2 rounded-xl shadow-xl bg-white flex flex-col items-center justify-center">
+        <div className="h-95/100 w-95/100 border-1 rounded-xl border-gray-300 border-dashed flex flex-col items-center justify-center">
+            <PhLegoSmiley :size="96" color="#9CA3AF" weight="thin" />
+            <p className="mt-5 text-gray-400 text-xl">Waiting for a task!</p>
+        </div>
+    </div>
+</template>
