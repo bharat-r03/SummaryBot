5c10fa95733c1f48d03edaaa7878f8093b3f8e3a
Dolphin xyz <bharat.raghavan03@gmail.com>
2025-08-11 23:31:07 -0400

    Add Docker integration `docker compose up` now launches frontend and backend for easy usage; also integrate front-back communication and backend-Ollama communication

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from summarybot!")
-
-
-if __name__ == "__main__":
-    main()
--- src/app/main.py
+++ src/app/main.py
@@ -21,7 +21,9 @@ app = FastAPI()
 
 origins = [
     "http://localhost",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080"
 ]
 
 app.add_middleware(
