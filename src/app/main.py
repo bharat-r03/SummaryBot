963f51a6fbd7212ec3c7e35c918f58922ac62988
Dolphin xyz <bharat.raghavan03@gmail.com>
2025-08-15 22:53:10 -0400

    Implement loading spinners for all processing steps

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, File, UploadFile, Request, Form
+from fastapi import FastAPI, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import Response, StreamingResponse, FileResponse
+from fastapi.responses import Response, StreamingResponse
 from summarize.summarize import create_chunked_summary, create_main_summary
 from audio.audio import text_to_audio
 import uuid
@@ -9,6 +9,7 @@ import shutil
 from typing import Annotated
 import base64
 import ollama
+import asyncio
 
 
 MODEL_DICT = {
@@ -40,14 +41,34 @@ app.add_middleware(
 )
 
 
+stage_dict = {"stage": ""}
+
+async def set_stage(stage: str):
+    stage_dict["stage"] = stage
+    await asyncio.sleep(0.5)
+
+
+async def update_stage():
+    while True:
+        status = stage_dict["stage"]
+        yield f"event: stageUpdate\ndata: {status}\n\n"
+        await asyncio.sleep(1)
+
+
+@app.get("/stream")
+async def stream_stage():
+    return StreamingResponse(update_stage(), media_type="text/event-stream")
+
+
 @app.post("/generate-test")
 async def generate_audio_test(files: list[UploadFile], voice: Annotated[str, Form()], summary_type: Annotated[str, Form()], model: Annotated[str, Form()]):
     sample_audio_path = "C:/Dev/SummaryBot/notebooks/audio_processing/test.wav"
     with open(sample_audio_path, "rb") as audio_file:
         summary_audio_bytes = audio_file.read()
-    
+
     summary_audio_bytes = base64.b64encode(summary_audio_bytes).decode("utf-8")
     headers = {'Content-Disposition': 'attachment; filename="test.wav"'}
+    await set_stage("processing_complete")
     return Response(summary_audio_bytes, headers=headers, media_type="audio/wav")
 
 
@@ -56,26 +77,33 @@ async def generate_audio(files: list[UploadFile], voice: Annotated[str, Form()],
     if not os.path.exists("tmpfiles"):
         os.mkdir("tmpfiles")
 
+    await set_stage("load_model")
     model_name = MODEL_DICT[model]
     current_model_list = [model["model"] for model in ollama.list().models]
     if not model_name in current_model_list:
         ollama.pull(model_name)
 
+    await set_stage("process_files")
     fpaths = []
     for file in files:
         fname = f"tmpfiles/tmp_file_{uuid.uuid4()}.pdf"
         with open(fname, "wb") as f:
             f.write(file.file.read())
         fpaths.append(fname)
+    
 
     chunk_summaries = []
+    await set_stage("summarize_files")
 
     for idx, fpath in enumerate(fpaths):
         print(f"Now processing file #{idx+1}!")
         file_summary = create_chunked_summary(fpath, model_name=model_name)
         chunk_summaries.append(file_summary)
 
+    await set_stage("summarize_main")
     main_summary = create_main_summary(chunk_summaries, model_name=model_name, summary_type=summary_type.lower())
+
+    await set_stage("text_to_audio")
     summary_audio_bytes = text_to_audio(script_text=main_summary, speaker_voice=VOICE_DICT[voice.lower()])
 
     for fpath in fpaths:
@@ -85,4 +113,5 @@ async def generate_audio(files: list[UploadFile], voice: Annotated[str, Form()],
 
     summary_audio_bytes = base64.b64encode(summary_audio_bytes).decode("utf-8")
     headers = {'Content-Disposition': 'attachment; filename="test.wav"'}
+    await set_stage("processing_complete")
     return Response(summary_audio_bytes, headers=headers, media_type="audio/wav")
