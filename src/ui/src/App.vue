2a20cdc81ac3c398c47c6c66e087fd3a66e5bd43
Dolphin xyz <bharat.raghavan03@gmail.com>
2025-08-16 23:56:04 -0400

    Add dark mode styles for baseboard and input modal

--- src/ui/src/App.vue
+++ src/ui/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import InputDialogBox from './components/InputDialogBox.vue';
 import OutputDialogBox from './components/OutputDialogBox.vue';
+import { isDarkMode } from './components/varStore';
 </script>
 
 <template>
-  <div id="baseboard" className="grid grid-cols-2 h-screen w-screen">
+  <div id="baseboard" :class="[ isDarkMode ? 'bg-gray-900' : 'bg-white', 'grid grid-cols-2 h-screen w-screen' ]">
     <div id="inputs" className="flex items-center justify-center ml-20">
       <InputDialogBox />
     </div>
